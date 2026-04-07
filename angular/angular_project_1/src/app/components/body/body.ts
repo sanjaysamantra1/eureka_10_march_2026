53c3e7874663be9f94716e790f636c11fea4b551
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-04-07 07:41:19 +0530

    april 6

--- angular/angular_project_1/src/app/components/body/body.ts
+++ angular/angular_project_1/src/app/components/body/body.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { Databinding } from '../databinding/databinding';
 import { DatabindingAssignment } from '../databinding-assignment/databinding-assignment';
 import { Directives } from '../directives/directives';
+import { MyModal } from '../my-modal/my-modal';
 
 @Component({
   selector: 'app-body',
   imports: [
     // Databinding,
     // DatabindingAssignment
-    Directives
+    // Directives
+    MyModal,
   ],
   templateUrl: './body.html',
   styleUrl: './body.css',
--- angular/angular_project_1/src/app/components/directives/directives.ts
+++ angular/angular_project_1/src/app/components/directives/directives.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-directives',
-  imports: [CommonModule, FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './directives.html',
   styleUrl: './directives.css',
 })
@@ -16,11 +16,60 @@ export class Directives {
   carArr = [];
 
   employees = [
-        { id: 1, name: 'Amit Sharma', role: 'Manager', salary: 85000, status: 'Active', gender: 'male' },
-        { id: 2, name: 'Priya Verma', role: 'Developer', salary: 65000, status: 'Active', gender: 'female' },
-        { id: 3, name: 'Rahul Mehta', role: 'Tester', salary: 38000, status: 'Inactive', gender: 'male' },
-        { id: 4, name: 'Sneha Iyer', role: 'Developer', salary: 42000, status: 'Inactive', gender: 'female' },
-        { id: 5, name: 'Karan Singh', role: 'Manager', salary: 52000, status: 'Active', gender: 'male' },
-        { id: 6, name: 'Neha Gupta', role: 'Tester', salary: 72000, status: 'Active', gender: 'female' }
+    {
+      id: 101,
+      name: 'Amit Sharma',
+      role: 'Manager',
+      salary: 85000,
+      status: 'Active',
+      gender: 'male',
+    },
+    {
+      id: 102,
+      name: 'Priya Verma',
+      role: 'Developer',
+      salary: 65000,
+      status: 'Active',
+      gender: 'female',
+    },
+    {
+      id: 103,
+      name: 'Rahul Mehta',
+      role: 'Tester',
+      salary: 38000,
+      status: 'Inactive',
+      gender: 'male',
+    },
+    {
+      id: 104,
+      name: 'Sneha Iyer',
+      role: 'Developer',
+      salary: 42000,
+      status: 'Inactive',
+      gender: 'female',
+    },
+    {
+      id: 105,
+      name: 'Karan Singh',
+      role: 'Manager',
+      salary: 52000,
+      status: 'Active',
+      gender: 'male',
+    },
+    {
+      id: 106,
+      name: 'Neha Gupta',
+      role: 'Tester',
+      salary: 72000,
+      status: 'Active',
+      gender: 'female',
+    },
   ];
+
+  myStyle1 = { color: 'green', backgroundColor: 'red', border: '3px dotted yellow' };
+  myStyle2 = { color: 'red', backgroundColor: 'green', border: '3px dotted blue' };
+
+  myFunction() {
+    return this.num % 2 == 0 ? this.myStyle1 : this.myStyle2;
+  }
 }
